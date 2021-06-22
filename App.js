824db289c2b291d9a97cd996114c15267eb6447c
alexkinsey <alexanderkinsey@icloud.com>
2021-06-22 14:26:49 +0100

    basic flex alignment

--- App.js
+++ App.js
@@ -20,18 +20,25 @@ export default function App() {
     <View style={{
       backgroundColor: 'white',
       flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-evenly',
+      alignItems: 'center',
     }}>
       <View style={{
         backgroundColor: 'dodgerblue',
-        flex: 2,
+        width: 100,
+        height: 100,
+        alignSelf: 'flex-start'
       }} />
       <View style={{
         backgroundColor: 'gold',
-        flex: 1,
+        width: 100,
+        height: 100,
       }} />
       <View style={{
         backgroundColor: 'red',
-        flex: 1,
+        width: 100,
+        height: 100,
       }} />
     </View>
   );
