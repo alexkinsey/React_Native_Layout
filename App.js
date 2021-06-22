5f0d8ac8332661a1a2411af9b7017ef3238b6daa
alexkinsey <alexanderkinsey@icloud.com>
2021-06-22 14:19:19 +0100

    created views with flex

--- App.js
+++ App.js
@@ -7,23 +7,40 @@ export default function App() {
   const { landscape } = useDeviceOrientation();
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          width: '100%',
-          height: landscape ? '100%' : '30%',
-        }}
-      ></View>
-      <StatusBar style="auto" />
-    </SafeAreaView>
+    // <SafeAreaView style={styles.container}>
+    //   <View
+    //     style={{
+    //       backgroundColor: 'blue',
+    //       width: '100%',
+    //       height: landscape ? '100%' : '30%',
+    //     }}
+    //   ></View>
+    //   <StatusBar style="auto" />
+    // </SafeAreaView>
+    <View style={{
+      backgroundColor: 'white',
+      flex: 1,
+    }}>
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        flex: 2,
+      }} />
+      <View style={{
+        backgroundColor: 'gold',
+        flex: 1,
+      }} />
+      <View style={{
+        backgroundColor: 'red',
+        flex: 1,
+      }} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    // flex: 1,
+    // backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
